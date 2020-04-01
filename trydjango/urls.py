1e946b6390251dc48a79c3a4245166bdd91e6f8c
saeedfalowo <saeedfalowo@users.github.com>
2020-04-02 00:30:59 +0100

    Progression uptil DJANGO MODEL FORMS section at t=2:06:50

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view
-
+from products.views import product_detail_view
 urlpatterns = [
 	path('', home_view, name='home'),
 	path('contact/', contact_view, name='home'),
+    path('product/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
