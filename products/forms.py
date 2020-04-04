39d98dabc882baa338aeaf41f08e6abbb9c552de
saeedfalowo <saeedfalowo@users.github.com>
2020-04-04 23:38:23 +0100

    Progression uptil DYNAMIC URL ROUTING section at t=2:51:44

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,70 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+	title = forms.CharField(
+		label='Newerer Title',
+		widget=forms.TextInput(
+			attrs={
+			"placeholder": "Whats your title?"
+			}
+			)
+		)
+	description = forms.CharField(
+		required=False,
+		widget=forms.Textarea(
+			attrs={
+			"class": "new-class-name two",
+			"placeholder": "You dont really have to type anything, you can skip if you want...",
+			"id":"my-id-for-textarea",
+			"rows": 20,
+			"cols": 120
+			}
+			)
+		)
+
+	price = forms.DecimalField(initial=0.99)
+	email = forms.EmailField(widget=forms.TextInput(
+		attrs={"placeholder": "anything@something.somethingelse"}))
 	class Meta:
 		model 	= Product
 		fields	= ['title','description','price']
+
+	def clean_title(self, *args, **kwargs):
+		# get the title
+		title = self.cleaned_data.get("title")
+		# to check if the required char combination, "cfe" exists in title
+		if "CFE" in title:
+			raise forms.ValidationError("This is not a valid title")
+		return title
+
+	def clean_email(self, *args, **kwargs):
+		# get the email
+		title = self.cleaned_data.get("email")
+		# to check if the required char combination, "cfe" exists in title
+		# if not "@" in email:
+		if not email.endswith("edu"):
+			raise forms.ValidationError("This is not a valid email")
+		return email
+
+class RawProductForm(forms.Form):
+	title 		= forms.CharField(
+		label='Newerer Title',
+		widget=forms.TextInput(
+			attrs={
+			"placeholder": "Whats your title?"
+			}
+			)
+		)
+	description = forms.CharField(
+		required=False,
+		widget=forms.Textarea(
+			attrs={
+			"class": "new-class-name two",
+			"placeholder": "You dont really have to type anything, you can skip if you want...",
+			"id":"my-id-for-textarea",
+			"rows": 20,
+			"cols": 120
+			}
+			)
+		)
+	price		= forms.DecimalField(initial=0.99)
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Import the Product class
 from .models import  Product
+from .forms import ProductForm, RawProductForm
 # Create your views here.
 def product_detail_view(request):
 	obj = Product.objects.get(id=1)
@@ -15,9 +16,12 @@ def product_detail_view(request):
 	# return render(request, "product/detail.html", context)
 	return render(request, "products/product_detail.html", context)
 
-from .forms import ProductForm
 def product_create_view(request):
-	form = ProductForm(request.POST or None)
+	obj = Product.objects.get(id=1)
+	form = ProductForm(request.POST or None, instance=obj)
+	# initial_data = {'title': "My intial data"}
+	# form = ProductForm(request.POST or None, initial=initial_data)
+	# form = ProductForm(request.POST or None)
 	if form.is_valid():
 		form.save()
 		form = ProductForm()
@@ -27,3 +31,30 @@ def product_create_view(request):
 	}
 	# return render(request, "product/detail.html", context)
 	return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+# 	if request.method == "POST"
+# 		# print(request.GET)
+# 		# print(request.POST)
+# 		my_new_title = request.POST.get('title')
+# 		print(my_new_title)
+# 		# Product.objects.create(title=my_new_title)
+# 	context = {}
+# 	return render(request, "products/product_create_raw.html", context)
+
+# def product_create_view(request):
+# 	my_form = RawProductForm()
+# 	if request.method == "POST":
+# 		my_form = RawProductForm(request.POST)
+# 		if my_form.is_valid():
+# 			# Now the data is good
+# 			print(my_form.cleaned_data)
+# 			Product.objects.create(**my_form.cleaned_data)
+# 		else:
+# 			print(my_form.errors)
+
+# 	my_form = RawProductForm()
+# 	context = {
+# 		"form": my_form
+# 	}
+# 	return render(request, "products/product_create_djangoraw.html", context)
